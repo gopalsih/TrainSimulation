3a364a0aeac49f20a7e9a093efbe9a061ab047b3
hemant <abhigopals103@gmail.com>
2022-02-22 17:00:47 -0500

    removed unused files

--- src/signal.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIGNAL_H_INCLUDED
-#define SIGNAL_H_INCLUDED
-
-struct signalObject {
-    bool present;
-};
-
-#endif // SIGNAL_H_INCLUDED
---
+++ src/trainManager.h
@@ -0,0 +1,7 @@
+#ifndef TRAINMANAGER_H_INCLUDED
+#define TRAINMANAGER_H_INCLUDED
+
+#include "trafficObject.h"
+
+
+#endif // TRAINMANAGER_H_INCLUDED
