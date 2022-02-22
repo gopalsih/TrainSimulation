0c81a309999b7cda10057fc704375d50b8fdda10
hemant <abhigopals103@gmail.com>
2022-02-21 22:20:04 -0500

    grid params definition

---
+++ src/grid.h
@@ -0,0 +1,32 @@
+#ifndef GRID_H_INCLUDED
+#define GRID_H_INCLUDED
+
+#include "gridSquare.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define GRID_SIZE 12
+
+class grid {
+public:
+    gridSquare gridArray[GRID_SIZE][GRID_SIZE];
+    int mNumTrains;
+    void readTrackFile(void);
+    void readTrainFile(void);
+    void readSinalFile(void);
+    void computePaths(void);
+    bool checkCollision(void);
+    grid(){};
+private:
+    std::vector<trainObject>trainArray;
+    std::vector<int> string2Int(std::string input);
+    void computePath(trainObject *train);
+    int findNearestSignal(trainObject *train, int pos);
+};
+
+
+
+
+#endif // GRID_H_INCLUDED
